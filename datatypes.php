0dba0d6c241f06485ee979b68e1fbb59d7f7118a
usernameshouldbeavailable <avinash@edunetfoundation.org>
2021-12-01 09:45:42 +0530

    learning about datatypes

---
+++ datatypes.php
@@ -0,0 +1,12 @@
+<?php
+
+    // basic data types
+
+    $name = "Avinash"; // string
+    $age = 400; // int
+    $marks = 8.5; // float
+    $is_indian = true; // boolean
+    $null_value = null; // null
+    $cities = array("Mumbai", "Delhi"); // array
+
+?>
