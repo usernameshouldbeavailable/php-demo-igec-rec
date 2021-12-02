586b0a046b8743b0e50b2e5fe3b2e8b412a6bf61
usernameshouldbeavailable <avinash@edunetfoundation.org>
2021-12-02 09:22:17 +0530

    example of string concatation

---
+++ stringcon.php
@@ -0,0 +1,21 @@
+<?php
+
+    $firstName = "Avinash";
+
+    $lastName = "Seth";
+
+    echo $firstName . " " . $lastName;
+
+    // Avinash[SPACE]Seth
+
+    echo "<br />";
+
+    echo "Hi, my name is " . $firstName . " " . $lastName;
+
+    // Hi, my name is Avinash[SPACE]Seth
+
+    echo "<br />";
+
+    echo "Hi, my name is {$firstName} {$lastName}";
+
+?>
