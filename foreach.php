092bc9a6684e77eae24869453baa9d00e9fedd74
usernameshouldbeavailable <avinash@edunetfoundation.org>
2021-12-02 09:37:17 +0530

    example of foreach loop

---
+++ foreach.php
@@ -0,0 +1,23 @@
+<?php
+
+    $following = array(
+        "@microsoft",
+        "@facebook",
+        "@instagram",
+        "@edunetfoundation",
+        "@sap",
+        "@twitter",
+        "@zuck",
+        "@github",
+        "@mozilla",
+        "@apple"
+    );
+
+    foreach($following as $follow)
+    {
+
+        echo "You are following <strong>{$follow}</strong> <br />";
+
+    }
+
+?>
