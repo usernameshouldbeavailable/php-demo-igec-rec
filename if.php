2a53e3154d8bde2bda05fb1e117578a0836a84bf
usernameshouldbeavailable <avinash@edunetfoundation.org>
2021-12-02 08:51:24 +0530

    if statement

---
+++ if.php
@@ -0,0 +1,19 @@
+<?php
+
+    $age = 18;
+
+    if ( $age >= 18 )
+    {
+
+        // when age is more than 18
+
+        echo "You can vote!";
+
+    }
+
+    // if (true)
+    // {
+        // run this block of code
+    // }
+
+?>
