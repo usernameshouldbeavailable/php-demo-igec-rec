3aa3d8ec38de625c5cb76e70e05041a72cd90f7c
usernameshouldbeavailable <avinash@edunetfoundation.org>
2021-12-03 09:15:15 +0530

    explained the problem with get

---
+++ problemwithget.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Problem with Get</title>
+</head>
+<body>
+    <form action="">
+        <p><label for="">Username</label></p>
+        <p><input type="text" name="username" /></p>
+        <p><label for="">Password</label></p>
+        <p><input type="password" name="password" /></p>
+        <p><input type="submit" value="Login"></p>
+    </form>
+</body>
+</html>
