488a5ec98295a10a1fe2b2a83e29fb74124f770c
usernameshouldbeavailable <avinash@edunetfoundation.org>
2021-12-15 08:59:32 +0530

    added comments

--- session.php
+++ session.php
@@ -1,9 +1,12 @@
 <?php
 
+    // to use session, use this function
     session_start();
 
+    // to set session value
     $_SESSION["useremail"] = "avinash@edunetfoundation.org";
 
+    // to get session value
     echo $_SESSION["useremail"];
 
 ?>
