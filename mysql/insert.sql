4c8e8d2d47959ee6905d9bb3c44bac161551495b
usernameshouldbeavailable <avinash@edunetfoundation.org>
2021-12-23 08:51:30 +0530

    Insert Query

---
+++ mysql/insert.sql
@@ -0,0 +1,23 @@
+-- insert single row
+
+INSERT INTO `mytable`
+    (`name`, 
+    `phone`, 
+    `email`, 
+    `address`, 
+    `list`, 
+    `country`, 
+    `region`, 
+    `postalZip`, 
+    `text`, 
+    `numberrange`, 
+    `currency`, 
+    `alphanumeric`
+    ) VALUES (
+        'Avinash',
+        '888',
+        'avinash@edunetfoundation.org',
+        'None',
+        '12',
+        'India',
+        'MP','123456','Hello world','5','$15','asdf54');
