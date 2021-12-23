1ce9e233a8f9432c669c35bc5a3e4a265e550cb1
usernameshouldbeavailable <avinash@edunetfoundation.org>
2021-12-23 09:35:35 +0530

    connecting to db using mysqli

---
+++ crud/connect_db_with_mysqli.php
@@ -0,0 +1,16 @@
+<?php
+
+
+    $databaseHost = "localhost"; // facebook.com instagram.com microsoft.com
+
+    $databaseUser = "root";
+
+    $databasePass = "";
+
+    $databaseName = "avinash";
+
+    $mysqli = mysqli_connect($databaseHost, $databaseUser,$databasePass, $databaseName);
+
+    print_r($mysqli);
+
+?>
