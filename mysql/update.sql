9727d63886ff7d21bd7e19158cd0955277631bd4
usernameshouldbeavailable <avinash@edunetfoundation.org>
2021-12-23 08:57:46 +0530

    update query

---
+++ mysql/update.sql
@@ -0,0 +1,7 @@
+-- update email address of user with id = 2
+
+UPDATE mytable SET email = 'avinash@edunetfoundation.org' WHERE id = 2;
+
+-- update multiple columns of user with id = 2
+
+UPDATE mytable SET email = 'avinash@edunetfoundation.org', name = 'Avinash Seth' WHERE id = 2;
